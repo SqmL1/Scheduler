c5b67345babb2d469812e9bd049f3bedf68f506f
Daren <darenbach2020@gmail.com>
2024-08-11 20:22:04 -0500

    added background

---
+++ SCHEDULER/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h-screen w-screen gradient">
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .gradient{
+        
+        background-color: rgb(209, 213, 219);
+        background-image: radial-gradient(at 89% 5%, rgb(254, 215, 170) 0, transparent 54%);
+    }
+  
+</style>
