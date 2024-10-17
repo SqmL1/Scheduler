c30ed929f2d519c40c2f0cc619834ad2359092cc
Daren <darenbach2020@gmail.com>
2024-10-16 23:47:13 -0500

    random

--- SCHEDULER/pages/index.vue
+++ SCHEDULER/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-full w-full gradient">
         <div class="flex px-5 py-5">
-            <h1 class="rubik text-2xl flex-auto"><span class="text-orange-400">My</span>Schedular</h1>
+            <h1 class="rubik text-2xl flex-auto"><span class="text-orange-400">My</span>Scheduler</h1>
             
             <button class="font-sans font-bold px-2"><NuxtLink to="/signup">Log in</NuxtLink></button>
             <button class="bg-black text-white rounded-full font-sans font-bold px-4 py-2"><NuxtLink to="/signup">Sign up</NuxtLink></button>      
